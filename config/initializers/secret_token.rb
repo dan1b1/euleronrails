1b92249cf67187e5c3a201371fd5682728c6b854
Daniel Braginsky <danbraginsky@gmail.com>
2012-08-14 23:25:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euleronrails::Application.config.secret_token = '022fc6cd4d3f4f123db13dac850f41cad3f32716f13dce087f115b7e3a4da3d8fe0ce5b70f979dec59154634d3780bed48f3a77474255ace0d8251479de02742'
